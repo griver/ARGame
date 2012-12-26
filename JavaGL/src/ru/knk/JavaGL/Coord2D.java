2e17401056ca763484f9f19fc8af7aa7f02ebb79
vasily-knk <Vasya@Pangaea.(none)>
2012-12-27 01:07:12 +0400

    Renderer update

---
+++ JavaGL/src/ru/knk/JavaGL/Coord2D.java
@@ -0,0 +1,10 @@
+package ru.knk.JavaGL;
+
+public class Coord2D {
+    Coord2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float x, y;
+}
---
+++ JavaGL/src/ru/knk/JavaGL/GameRun.java
@@ -0,0 +1,8 @@
+package ru.knk.JavaGL;
+
+public class GameRun implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
